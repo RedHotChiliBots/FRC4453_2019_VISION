27f54c5e194f45efe5e869de5b7bb46084f0f817
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-02-01 13:41:07 -0500

    Fixed syntax error.

--- libs/install_opencv.sh
+++ libs/install_opencv.sh
@@ -60,7 +60,7 @@ sudo apt-get install -y unzip wget
 wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
 unzip ${OPENCV_VERSION}.zip
 rm ${OPENCV_VERSION}.zip
-if [! -e OpenCV]; then
+if [ ! -e OpenCV]; then
     mv opencv-${OPENCV_VERSION} OpenCV
 fi
 cd OpenCV
