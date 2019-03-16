5ff4202dbd7a2e186bf5960c1bc86e1ff20067b1
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-03-16 17:52:10 -0400

    Fix pixyrec 2.

--- pixyrec.cpp
+++ pixyrec.cpp
@@ -11,15 +11,16 @@
 
 
 int main() {
-    Pixy2 pixy();
+    auto pixy = new Pixy2();
 
-    pixy.m_link.stop();
+    pixy->m_link.stop();
 
-    cv::VideoWriter out("pixy.avi");
-    
+    cv::VideoWriter out;
+    out.open("pixy.avi");
+
     for(size_t i = 0; i < 600; i++) {
         uint8_t* bayer = nullptr;
-        pixy.m_link.getRawFrame(&bayer);
+        pixy->m_link.getRawFrame(&bayer);
 
         cv::Mat m_bayer(PIXY2_RAW_FRAME_HEIGHT, PIXY2_RAW_FRAME_WIDTH, CV_8U, bayer);
 
