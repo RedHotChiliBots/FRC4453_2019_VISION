6aa97814bc0542c52aee5adce0dfd4a16f1f296b
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-03-16 18:44:29 -0400

    TRY. and fix fps.

--- pixyrec.cpp
+++ pixyrec.cpp
@@ -20,7 +20,7 @@ int main() {
     pixy->m_link.stop();
 
     cv::VideoWriter out;
-    out.open("pixy.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 60.0, cv::Size(316, 208), true);
+    out.open("pixy.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30.0, cv::Size(316, 208), true);
     if(!out.isOpened()) {
         std::cout << "Cannot open file!" << std::endl;
         return -2;
@@ -33,7 +33,7 @@ int main() {
         cv::Mat m_bayer(PIXY2_RAW_FRAME_HEIGHT, PIXY2_RAW_FRAME_WIDTH, CV_8U, bayer);
 
         cv::Mat frame;
-        cv::cvtColor(m_bayer, frame, cv::ColorConversionCodes::COLOR_BayerGR2BGR, 3);
+        cv::cvtColor(m_bayer, frame, cv::ColorConversionCodes::COLOR_BayerRG2BGR, 3);
 
         if(i == 0) {
             std::cout << "Real Size: " << frame.cols << ", " << frame.rows << std::endl;
