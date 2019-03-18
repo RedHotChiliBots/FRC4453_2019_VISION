c22d180457ecd41bb7e93982cbf19fa937a006ed
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-03-18 17:11:46 -0400

    More speed.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include <cmath>
 #include <algorithm>
 #include <future>
-#include <sstream>
-#include <optional>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/rotating_file_sink.h>
-#include "spdlog/fmt/bin_to_hex.h"
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
