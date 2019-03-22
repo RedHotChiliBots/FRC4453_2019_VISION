5172ef54eb3220377770a45ba3e3adddd51f3e45
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2019-03-22 16:01:48 -0400

    Appemt to fix optional.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/rotating_file_sink.h>
+#include <optional>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
